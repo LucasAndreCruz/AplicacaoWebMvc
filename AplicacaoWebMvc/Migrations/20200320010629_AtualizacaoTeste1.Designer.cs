d9d4d11adb8746afe13e25181cca772a0a40c4f9
LUCAS ANDRÉ <lucas_andre_spfc@hotmail.com>
2020-03-19 22:08:19 -0300

    Adicionando Migration de teste 1

--- AplicacaoWebMvc/Migrations/20200320010629_AtualizacaoTeste1.Designer.cs
+++ AplicacaoWebMvc/Migrations/20200320010629_AtualizacaoTeste1.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using AplicacaoWebMvc.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace AplicacaoWebMvc.Migrations
 {
     [DbContext(typeof(AplicacaoWebMvcContext))]
-    partial class AplicacaoWebMvcContextModelSnapshot : ModelSnapshot
+    [Migration("20200320010629_AtualizacaoTeste1")]
+    partial class AtualizacaoTeste1
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ AplicacaoWebMvc/Migrations/20200320010629_AtualizacaoTeste1.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AplicacaoWebMvc.Migrations
+{
+    public partial class AtualizacaoTeste1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Contato_PessoaId",
+                table: "Contato");
+
+            migrationBuilder.DropColumn(
+                name: "Nome",
+                table: "Contato");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Logradouro",
+                table: "Endereco",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Cidade",
+                table: "Endereco",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Cep",
+                table: "Endereco",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Bairro",
+                table: "Endereco",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Contato_PessoaId",
+                table: "Contato",
+                column: "PessoaId",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Contato_PessoaId",
+                table: "Contato");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Logradouro",
+                table: "Endereco",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Cidade",
+                table: "Endereco",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Cep",
+                table: "Endereco",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Bairro",
+                table: "Endereco",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AddColumn<int>(
+                name: "Nome",
+                table: "Contato",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Contato_PessoaId",
+                table: "Contato",
+                column: "PessoaId");
+        }
+    }
+}
